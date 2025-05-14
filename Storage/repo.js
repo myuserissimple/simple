2c02fe072b42bb8e32f0e515ac103319d9ef8635
Simple <195865945+myuserissimple@users.noreply.github.com>
2025-05-15 00:50:37 +0100

    Create repo.js

---
+++ Storage/repo.js
@@ -0,0 +1,40 @@
+{
+    "name": "SimpleXRepo",
+    "identifier": "SimpleXServer",
+    "subtitle": "by simple_00",
+    "iconURL": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1f80c78a-4070-4082-85ed-f2d4cf3894db/d88y06v-9bf586ed-3aa1-42b8-8ca5-343fc26843a2.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzFmODBjNzhhLTQwNzAtNDA4Mi04NWVkLWYyZDRjZjM4OTRkYlwvZDg4eTA2di05YmY1ODZlZC0zYWExLTQyYjgtOGNhNS0zNDNmYzI2ODQzYTIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.3ztis7iKYyTWcoutMZaydFV7NmDooZvh22emcS9OqEk",
+    "website": "https://discord.gg/Kr2pvPfEUR",
+    "sourceURL": "https://simplexserver.github.io/repo.json",
+    "tintColor": "FFC300",
+    "apps": [
+        {
+            "name": "Delta",
+            "bundleIdentifier": "com.gloop.deltamobile",
+            "developerName": "Delta",
+            "version": "2.642.637",
+            "versionDate": "2024-10-02",
+            "downloadURL": "https://download2390.mediafire.com/aph1h05xposgfN055vTTLKt649tOHkxVZsB0sWu4sc_z41tmlpXaffdZ4OAK48_bqlBSaLNer983_eocyZtk6zeZ73pcHKDlAg9jmS_VHk23FIrRSLjAUFY2rEckMGnME-TU3C8g5H27-w4q8Hcdr8d66OV0_i6FTOzj51swz0mvZA/45hxgvuc1zn3aw8/Delta-2.642.637.ipa",
+            "localizedDescription": "Roblox Executor",
+            "iconURL": "https://simplexserver.github.io/logo/roblox.png",
+            "tintColor": "FFC300",
+            "size": 209715200,
+            "screenshotURLs": [
+                "https://deltaexploits.gg/_next/image?url=%2Fassets%2Fandroid.webp&w=1920&q=100"
+            ]
+        },
+
+  
+        {
+            "name": "Cash App",
+            "bundleIdentifier": "Cash-App-by-ivglitched",
+            "developerName": "ivglitched",
+            "version": "1.0",
+            "versionDate": "2024-10-02",
+            "downloadURL": "https://github.com/simplexserver/simplexserver.github.io/raw/refs/heads/main/ipas/Cash%20App_1.0_1726727501.ipa",
+            "localizedDescription": "Visual Infinite Money",
+            "iconURL": "https://simplexserver.github.io/logo/cashapp.png",
+            "tintColor": "FFC300",
+            "size": 1782272
+        },
+   ]
+}
